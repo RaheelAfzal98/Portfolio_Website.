140b93f6328e57269dcb26846cc990fa8ae9e56b
RaheelAfzal98 <infotime8123@gmail.com>
2024-12-29 22:15:39 +0500

    Deployment Changes.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import Navbar from './Navbar'
+// import Navbar from './Navbar'
 
 const Hero = () => {
   return (
     <div id="hero" className='min-h-screen bg-no-repeat bg-[url(/banner_bg.png)] bg-cover'
     style={{backgroundSize: "23%", backgroundPosition:"left 200px top 180px"}}
     >
-        <Navbar/>
         <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
             <div className='hidden lg:block'></div>
             <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center text-white'>
             <div>
-                <p data-aos="zoom-in-up">I'm</p>
-                <p data-aos="zoom-in-up">Muhammad</p>
-                <p data-aos="zoom-in-up"  >Raheel</p>
+                <p data-aos='zoom-in-up'>I am</p>
+                <p data-aos='zoom-in-up'>Muhammad</p>
+                <p data-aos='zoom-in-up'>Raheel</p>
             </div>    
          </div>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className='container pt-8'>
-        <div className='flex justify-between items-center'>
+    <div className='container pt-8 sticky top-0'>
+        <div className='flex justify-between  items-center'>
                 <div className='text-xl font-medium'>Muhammad Raheel</div>
                 <ul className='gap-10 lg:gap-16 hidden md:flex'>
                     <li className='menuLink'><a href="#hero">Home</a></li>
@@ -21,4 +21,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
